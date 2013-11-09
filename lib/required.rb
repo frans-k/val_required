b69efa640ed798327d581554188f1771cefa070f
Frans Krojegård <frans.krojegard@mittmedia.se>
2013-11-09 19:26:34 +0100

    Refactored code.

--- lib/required.rb
+++ lib/required.rb
@@ -1,24 +1,29 @@
+# TODO: remove dependency
 require 'active_support/core_ext/object/blank'
 
 module Required
-  def required! val=nil
-    if block_given?
-      yield(Dummy.new(self))
-    elsif self.class == Object
-      puts fail "argument is blank!" if val.blank?
-    else
-      puts fail "object is blank" if self.blank?
+  module Method
+    def required! val=nil
+      if block_given?
+        yield(RequiredHelper.new(self))
+      elsif self.class == Object
+        raise BlankError.new("argument is blank!") if val.blank?
+      else
+        raise BlankError.new("object is blank") if self.blank?
+      end
     end
-    true
   end
 
-  class Dummy < Struct.new(:obj)
+  class RequiredHelper < Struct.new(:obj)
     def method_missing method, *args
       obj.send(method, *args).required!
     end
   end
+
+  class BlankError < StandardError
+  end
 end
 
 class Object
-  include Required
+  include Required::Method
 end
