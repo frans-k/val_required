a42d139ece73d11d36888f5d25a2582c1765a74f
Frans Krojegård <frans.krojegard@mittmedia.se>
2013-11-12 22:18:56 +0100

    Additional specs.

--- spec/required_spec.rb
+++ spec/required_spec.rb
@@ -20,10 +20,35 @@ describe ValRequired do
   it { fails nil }
   it { fails "" }
 
+  it "works with assignment" do
+    m = "moo".required!
+    expect(m).to eq "moo"
+
+    expect { required!("") }.to raise_error
+
+    m = required!("moo")
+    expect(m).to eq "moo"
+  end
+
   it "takes a block" do
     user = OpenStruct.new(moo: "mo", fish: "")
-    expect { user.required!(&:fish) }.to raise_error
 
+    expect { user.required!(&:fish) }.to raise_error
     expect { user.required!(&:moo) }.to_not raise_error
+  end
+
+  it "takes an assignment block (config style)" do
+    config = OpenStruct.new
+    required! do
+      config.moo = "moo"
+    end
+
+    expect(config.moo).to eq "moo"
+
+    expect do
+      config.required! do |c|
+        c.moo = ""
+      end
+    end.to raise_error
   end
 end
